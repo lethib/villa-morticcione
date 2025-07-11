6decd04862fe6d7b94632261fc13c620feca47eb
Thibault <thibault.mroz@faks.co>
2025-07-11 20:38:25 +0200

    feedbacks

--- src/app/calendar/_components/CalendarMonth.tsx
+++ src/app/calendar/_components/CalendarMonth.tsx
@@ -174,6 +174,7 @@ export const CalendarMonth = ({ monthKey }: { monthKey: string }) => {
               <X
                 size={38}
                 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-black"
+                aria-label="Reserved"
               />
             )}
           </div>
