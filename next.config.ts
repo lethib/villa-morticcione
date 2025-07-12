3a3fb34680603cf15d5e647b5e55a6dde5703694
Thibault <thibault.mroz@faks.co>
2025-07-12 19:19:30 +0200

    chore: remove base path

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/villa-morticcione",
-  assetPrefix: "/villa-morticcione",
   trailingSlash: true,
   images: {
     unoptimized: true,
